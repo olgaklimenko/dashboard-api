0f5bee04a84b28dde775212d89863610d8dd129f
Olga Klimenko <oklimenko92@mail.ru>
2024-08-06 21:24:44 +0700

    add App class

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Express } from "express";
+import { usersRouter } from "./users/users";
+import {Server} from "http";
+
+export class App {
+    app: Express;
+    server: Server;
+    port: number;
+
+    constructor() {
+        this.app = express();
+        this.port = 8000;
+    };
+
+    useRoutes() {
+        this.app.use('/users', usersRouter)
+    }
+
+    public async init() {
+        this.useRoutes();
+        this.server = this.app.listen(this.port);
+        console.log(`Server started on http://localhost:${this.port}`);
+    };
+
+}
