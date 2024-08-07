3cf2f34deba71492033724ddef3b83042c4f82a3
Olga Klimenko <oklimenko92@mail.ru>
2024-08-07 18:31:09 +0700

    use ContainerModule

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Container } from "inversify";
+import { Container, ContainerModule, interfaces } from "inversify";
 import { App } from "./app";
 import { ExceptionFilter } from "./errors/exeption.filter";
 import { LoggerService } from "./logger/logger.service";
@@ -6,13 +6,21 @@ import { UserController } from "./users/users.controller";
 import { TYPES } from "./types";
 import { ILogger } from "./logger/logger.interface";
 import { IExceptionFilter } from "./errors/exeption.filter.interface";
+import { IUserController } from "./users/users.controller.interface";
 
-const appContainer = new Container();
-appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService);
-appContainer.bind<UserController>(TYPES.UserController).to(UserController);
-appContainer.bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
-appContainer.bind<App>(TYPES.Application).to(App);
-const app = appContainer.get<App>(TYPES.Application)
-app.init();
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+    bind<ILogger>(TYPES.ILogger).to(LoggerService);
+    bind<IUserController>(TYPES.IUserController).to(UserController);
+    bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+    bind<App>(TYPES.Application).to(App);
+});
 
-export { app, appContainer };
+function bootstrap() {
+    const appContainer = new Container();
+    appContainer.load(appBindings);
+    const app = appContainer.get<App>(TYPES.Application)
+    app.init();
+    return { appContainer, app };
+}
+
+export const { app, appContainer } = bootstrap();
---
+++ src/users/users.controller.interface.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { BaseController } from "../common/base.controller";
+import { HTTPError } from "../errors/http-error.class";
+import { inject, injectable } from "inversify";
+import { TYPES } from "../types";
+import { ILogger } from "../logger/logger.interface";
+import 'reflect-metadata';
+
+export interface IUserController  {
+    register: (req: Request, res: Response, next: NextFunction) => void;
+
+    login: (req: Request, res: Response, next: NextFunction) => void;
+}
