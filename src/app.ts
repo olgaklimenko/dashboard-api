d469b22c9e7b3246cc4a15ec2917cd419d15a0fc
Olga Klimenko <oklimenko92@mail.ru>
2024-08-07 10:56:03 +0700

    add user controller

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,27 @@
 import express, { Express } from "express";
-import { usersRouter } from "./users/users";
 import {Server} from "http";
 import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
 
 export class App {
     app: Express;
     server: Server;
     port: number;
-    logger: LoggerService
+    logger: LoggerService;
+    userController: UserController;
 
-    constructor(logger: LoggerService) {
+    constructor(
+        logger: LoggerService,
+        userController: UserController
+    ) {
         this.app = express();
         this.port = 8000;
         this.logger = logger;
+        this.userController = userController;
     };
 
     useRoutes() {
-        this.app.use('/users', usersRouter)
+        this.app.use('/users', this.userController.router)
     }
 
     public async init() {
---
+++ src/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { BaseController } from "../common/base.controller";
+import { LoggerService } from "../logger/logger.service";
+
+export class UserController extends BaseController {
+    constructor( logger: LoggerService) {
+        super(logger);
+        this.bindRoutes([{
+            path: '/register', method: 'post', func: this.register
+        }, {
+            path: '/login', method: 'post', func: this.login
+        }
+            ])
+    }
+
+    register(req: Request, res: Response, next: NextFunction) {
+        this.ok(res, 'register');
+    }
+
+    login(req: Request, res: Response, next: NextFunction) {
+        this.ok(res, 'login');
+    }
+
+    
+
+
+}
