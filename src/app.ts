534d32eb6d0ae25f25aa77482acbe09e10414485
Olga Klimenko <oklimenko92@mail.ru>
2024-08-07 13:24:00 +0700

    add exceptions

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import {Server} from "http";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./users/users.controller";
+import { ExceptionFilter } from "./errors/exeption.filter";
 
 export class App {
     app: Express;
@@ -9,23 +10,31 @@ export class App {
     port: number;
     logger: LoggerService;
     userController: UserController;
+    exceptionFilter: ExceptionFilter;
 
     constructor(
         logger: LoggerService,
-        userController: UserController
+        userController: UserController,
+        exceptionFilter: ExceptionFilter
     ) {
         this.app = express();
         this.port = 8000;
         this.logger = logger;
         this.userController = userController;
+        this.exceptionFilter = exceptionFilter;
     };
 
     useRoutes() {
         this.app.use('/users', this.userController.router)
     }
 
+    useExceptionFilters() {
+        this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+    }
+
     public async init() {
         this.useRoutes();
+        this.useExceptionFilters();
         this.server = this.app.listen(this.port);
         this.logger.log(`Server started on http://localhost:${this.port}`);
     };
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { BaseController } from "../common/base.controller";
 import { LoggerService } from "../logger/logger.service";
+import { HTTPError } from "../errors/http-error.class";
 
 export class UserController extends BaseController {
     constructor( logger: LoggerService) {
@@ -18,10 +19,6 @@ export class UserController extends BaseController {
     }
 
     login(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'login');
+        next(new HTTPError(401, 'Auth error'));
     }
-
-    
-
-
 }
