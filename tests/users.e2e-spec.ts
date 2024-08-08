c7f6fb440d6d118b19750bf59d39287d131c8bed
Olga Klimenko <oklimenko92@mail.ru>
2024-08-08 17:55:46 +0700

    add e2e tests

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -10,12 +10,42 @@ beforeAll(async () => {
 });
 
 describe('Users e2e', () => {
-	it('Register - error', async () => {
+	it('Register - user exist error', async () => {
+		await request(application.app).post('/users/register').send({
+			email: 'testuser@example.com',
+			password: 'testpassword',
+			name: 'Test User',
+		});
 		const res = await request(application.app).post('/users/register').send({
-			email: 'example@example.com',
-			password: '1',
+			email: 'testuser@example.com',
+			password: 'testpassword',
+			name: 'Test User',
 		});
 		expect(res.statusCode).toBe(422);
+	});
+	it('Login - success', async () => {
+		const res = await request(application.app).post('/users/login').send({
+			email: 'testuser@example.com',
+			password: 'testpassword',
+		});
+		expect(res.body.token).not.toBeUndefined();
+	});
+	it('Login - wrong password error', async () => {
+		const res = await request(application.app).post('/users/login').send({
+			email: 'testuser@example.com',
+			password: 'wrong',
+		});
+		expect(res.statusCode).toBe(401);
+	});
+	it('Info - success', async () => {
+		const { body } = await request(application.app).post('/users/login').send({
+			email: 'testuser@example.com',
+			password: 'testpassword',
+		});
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${body.token}`);
+		expect(res.body.email).toEqual('testuser@example.com');
 	});
 });
 
